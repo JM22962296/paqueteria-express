6b97b3373e8b6d0f0a1e62f881782210149d3fe1
JM22962296 <pomeloromero@gmail.com>
2025-07-11 14:27:01 -0300

    Create data.js

---
+++ data.js
@@ -0,0 +1,8 @@
+// Estado compartido del paquete
+const paqueteEstado = {
+  ubicacion: [-51.6226, -69.2181],
+  destino: [-51.6200, -69.2150],
+  entregado: false,
+  enCiudad: false,
+  clienteRespondio: false
+};
